815eaa469c2a0444f4d702355c7cda09e29c740d
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2021-12-08 14:04:15 +0000

    working with array in the app main component

--- plan-picker/app.js
+++ plan-picker/app.js
@@ -1,7 +1,16 @@
 Vue.component('plan', {
     template: "#plan-template",
-    props: ["name"]
+    props: {
+        name: {
+            type: String,
+            required: true
+        }
+    },
 })
 new Vue({
-    el: "#app"
+    el: "#app",
+    data: function() {
+        return { plans: [{name: "The Hacker", id: 0}, {name: "The Curious", id: 1}, 
+        {name: "The Single", id: 2}] }
+    }
 })
